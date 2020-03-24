574576a10e21d31120cb80533963733d37076e5b
Gorka <gorka@540deg.com>
2020-03-24 10:00:34 +0100

    Improve single sign on kata

--- src/myservice/MyService.js
+++ src/myservice/MyService.js
@@ -1,4 +1,5 @@
 import Response from '../sso/Response'
+import SSOToken from '../sso/SSOToken'
 
 export default class MyService {
   constructor(registry) {
@@ -8,5 +9,15 @@ export default class MyService {
   handleRequest(request) {
     // TODO: check request has a valid SSOToken
     return new Response(`hello ${request.getName()}!`)
+  }
+
+  handleRegister(username, password) {
+    // TODO: register and return token
+    return
+  }
+
+  handleUnRegister(token) {
+    // TODO: unregister token
+    return
   }
 }
